1d59a98d6f3c7628e6bf16a57bdb8fd01ca8e74f
ronitshinde <rony.shinde753@gmail.com>
2022-12-29 16:29:26 +0530

    Comparing two lines

--- Program.cs
+++ Program.cs
@@ -5,11 +5,10 @@
         public static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to line comparison computation program");
+            //Console.WriteLine("Welcome to line comparison computation program");
             //Length.FindLength();
             //EqualityOfLines.TwoLines();
-            //Comparison.linecomparison();
-            //Compareto.GreatSmallEqual();
+            Compareto.GreatSmallEqual();
         }
     }
 }
